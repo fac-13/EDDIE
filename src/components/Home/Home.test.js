f57116cc452973bac456de8287f3bef6eb2f5ffd
i2xzy <yitzchokr@live.com>
2018-06-04 18:10:07 +0100

    initial commit, set up env, routes relates #2 #3

---
+++ src/components/Home/Home.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import Home from './Home';
+
+describe('test Home', () => {
+  test('displays Header', () => {
+    const { container } = render(<Home />);
+    const actual = container.querySelector('header');
+    expect(actual).toBeTruthy;
+  });
+});
