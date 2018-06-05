31eeaf2b3a8b2b8a136c1fe5e19a1f04f7826402
i2xzy <yitzchokr@live.com>
2018-06-05 17:22:17 +0100

    set up sass and style header relates #7

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,29 @@ import React from 'react';
 import propTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
+import './header.scss';
+
 const Header = ({ title, history }) => {
   return (
     <React.Fragment>
-      <header>
-        <button onClick={history.goBack}>Back</button>
-        <h1>{title}</h1>
+      <header className="header">
+        <div className="header__back" onClick={history.goBack}>
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M3.8 7L9.4 1.4L8 0L0 8L8 16L9.4 14.6L3.8 9H16V7H3.8Z"
+              fill="#3D4F51"
+            />
+          </svg>
+        </div>
+        <h1 className="header__title">{title}</h1>
       </header>
     </React.Fragment>
   );
