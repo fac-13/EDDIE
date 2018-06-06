affc8c974f1c0af1d6f87b206390e55f0d719bb7
i2xzy <yitzchokr@live.com>
2018-06-06 17:31:41 +0100

    add carousel and story data relates #7

--- src/components/Stories/Stories.js
+++ src/components/Stories/Stories.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 import Header from '../Header/Header';
+import Carousel from '../Carousel/Carousel';
+
+// Data for carousel
+const SlidesData = [
+  {
+    name: 'Peter',
+    age: '19',
+    symptoms: [
+      'Worried about his penis becoming smaller',
+      'ED the first time he tried to have sex with a partner 2 years ago',
+      'Masturbates compulsively 7-8 times a day',
+      'Does not feel as much pleasure as before when he masturbates'
+    ]
+  }
+];
 
 const Stories = () => (
   <React.Fragment>
     <Header title="Stories" />
+    <Carousel slides={SlidesData} />
   </React.Fragment>
 );
 
