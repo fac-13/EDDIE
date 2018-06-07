4e9afc5fd6efd198a1d13a8349a25764c994e48c
Nicos <nick.zonias@gmail.com>
2018-06-07 15:21:42 +0100

    merge caused with master

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../Button';
+import Button from '../Button/Button';
 import Header from '../Header/Header'
 
 const Resources = () => (
--- src/components/Theory/Theory.js
+++ src/components/Theory/Theory.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../Button';
+import Button from '../Button/Button';
 import Header from '../Header/Header';
 import './theory.scss';
 
@@ -8,19 +8,16 @@ const Theory = () => (
   <React.Fragment>
     <Header title="Theory" />
     <article className="theory__section">
-     <Button className="theory__button" link="causes">Causes</Button>
-     <Button className="theory__button" link="mechanism">Mechanism</Button>
-     <Button className="theory__button" link="myths">Myths</Button>
-     </article>
-     <svg  
-     className="theory__svg"
-     viewBox="0 0 1080 546" 
-     fill="none" 
-     xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M107.434 181.647H0V0C81.7091 7.06445 146.486 24.1143 198.169 37.718C238.586 48.3562 270.995 56.8867 297.234 56.8867L305.812 75.3982L107.434 181.647Z" transform="translate(0 1.05835) scale(3)" fill="#9ADA65"/>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M0 113.114V153.21H360V0C335.893 8.80103 311.835 18.5991 286.989 28.7175C208.146 60.8267 121.378 96.1636 0 113.114Z" transform="translate(0 86.3687) scale(3)" fill="#7CA5A9"/>
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M172.634 0V92.7683H0C29.918 77.3401 59.3933 61.1619 88.2316 45.3335C117.067 29.5063 145.266 14.0288 172.634 0Z" transform="translate(562.097 264.695) scale(3)" fill="#FD8489"/>
-    </svg>
-
+      <Button className="theory__button" link="causes">Causes</Button>
+      <Button className="theory__button" link="mechanism">Mechanism</Button>
+      <Button className="theory__button" link="myths">Myths</Button>
+    </article>
+      <svg className="theory__svg" viewBox="0 0 720 364" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M107.434 181.647H0V0C81.7091 7.06445 146.486 24.1143 198.169 37.718C238.586 48.3562 270.995 56.8867 297.234 56.8867L305.812 75.3982L107.434 181.647Z" transform="translate(0 0.705566) scale(2)" fill="#9ADA65"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M0 113.114V153.21H360V0C335.893 8.80103 311.835 18.5991 286.989 28.7175C208.146 60.8267 121.378 96.1636 0 113.114Z" transform="translate(0 57.5791) scale(2)" fill="#7CA5A9"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M172.634 0V92.7683H0C29.918 77.3401 59.3933 61.1619 88.2316 45.3335C117.067 29.5063 145.266 14.0288 172.634 0Z" transform="translate(374.731 176.463) scale(2)" fill="#FD8489"/>>
+      </svg>
+    </React.Fragment>
+)
 
 export default Theory;
