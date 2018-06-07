8266d26537a573fd61da97668b7edb25accd86ff
isnotafunction <ivana.cosgrove@gmail.com>
2018-06-07 10:28:04 +0100

    removes comment

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,8 +16,8 @@ const Home = () => (
       <p>
       This app has information and tools to help you improve your sex life.      
       </p>
-      </div>    
-    <div className="container__buttons">    
+  </div>    
+  <div className="container__buttons">    
      <Button className="button__home" link="stories">Stories</Button>
      <Button className="button__home" link="theory">Theory</Button>
      <Button className="button__home" link="resources">Resources</Button>
