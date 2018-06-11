04d42b693600facd5ab4df40945873d6d128b61e
i2xzy <yitzchokr@live.com>
2018-06-11 13:39:00 +0100

    start refactor carousel

--- src/components/App.js
+++ src/components/App.js
@@ -12,9 +12,47 @@ import Mechanism from './Mechanism/Mechanism';
 import Myths from './Myths/Myths';
 import Resources from './Resources/Resources';
 import ErrorPage from './ErrorPage/ErrorPage';
+import { ThemeProvider } from 'styled-components';
+
+const fontStack = [
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Segoe UI"',
+  'Roboto',
+  'Helvetica',
+  'Arial',
+  'sans-serif',
+  '"Apple Color Emoji"',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"'
+];
+
+const flexContainer = (direction, justify, align) => `
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  flex-direction: ${direction};
+  justify-content: ${justify};
+  align-items: ${align};
+  `;
 
 const App = () => (
-  <React.Fragment>
+  <ThemeProvider
+    theme={{
+      white: '#f8fefd',
+      black: '#3d4f51',
+      blue: '#b8f7f1',
+      darkBlue: '#1a5f67',
+      green: '#94d75b',
+      coral: '#fd8489',
+      slate: '#7ca5a9',
+      bodyFont: fontStack,
+      headerFont: ['Lato', ...fontStack],
+      flexContainer
+    }}
+  >
     <Switch>
       <Route exact path="/" component={Splash} />
       <Route exact path="/home" component={Home} />
@@ -26,7 +64,7 @@ const App = () => (
       <Route path="/resources" component={Resources} />
       <Route component={ErrorPage} />
     </Switch>
-  </React.Fragment>
+  </ThemeProvider>
 );
 
 export default App;
