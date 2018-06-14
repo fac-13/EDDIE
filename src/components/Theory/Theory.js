d697834832d1039a9576821c36910d05fd5d7cfe
Nicos <nick.zonias@gmail.com>
2018-06-14 21:52:19 +0100

    modify style for theory component

--- src/components/Theory/Theory.js
+++ src/components/Theory/Theory.js
@@ -27,7 +27,7 @@ ${params => params.theme.flexContainer('row', 'center', 'center')}
   height: 70px;
   background: ${params => params.theme.white};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  font-size: 1.5rem;
+  font-size: 1.2rem;
   text-align: center;
   text-transform: uppercase;
   text-decoration: none;
