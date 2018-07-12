0321916ba455185e387e730550317c470c59ff42
Nicos <nick.zonias@gmail.com>
2018-07-12 09:48:38 +0100

    minor corrections to user stories and causes

--- src/components/Stories/Stories.js
+++ src/components/Stories/Stories.js
@@ -6,13 +6,13 @@ import Carousel from '../Carousel/Carousel';
 const SlidesData = [
   {
     name: 'Daniel',
-    about: 'Married 25 years, has 1 child',
+    about: '47 years old',
     symptoms: [
       {
         type: 'physical',
         text: 'Smoker. Diabetic',
         intervention:
-          'Long–term smoking and Diabetes both cause erection difficulties. Daniel can discuss with his physician about medications that can help with his ED, alongside psychotherapy or couples counselling. '
+          'Long–term smoking and Diabetes both cause erection difficulties. Daniel can discuss with his physician about medications that can help with his ED, alongside psychotherapy or couples counselling.'
       },
       {
         type: 'psychological',
@@ -22,9 +22,9 @@ const SlidesData = [
       },
       {
         type: 'relational',
-        text: 'Impacting on relationship',
+        text: 'Impacting on his marriage',
         intervention:
-          'His wife, unaware of his worries and feelings, feels rejected and angry creating more tension and fights between them. In psychotherapy or couples therapy Daniel and his wife could use Sensate Focus Exercises as a way to reconnect intimately and explore a wide range of options to give and receive sexual pleasure with each other beyond erections and penetration. '
+          'His wife, unaware of his worries and feelings, feels rejected and angry creating more tension between them. In psychotherapy or couples therapy Daniel and his wife could use Sensate Focus Exercises as a way to reconnect intimately and explore a wide range of options to give and receive sexual pleasure with each other beyond erections and penetration.'
       }
     ]
   },
@@ -37,20 +37,20 @@ const SlidesData = [
         text:
           'Moved to London last year to study. He can get an erection but soon after he loses it',
         intervention:
-          'Jorge’s ED is related to feelings of anxiety and the expectations his sets to perform sexually for his partner. Also, moving to a new country, and in an new relationship can introduce a number of challenges that can increase feelings of anxiety and the fear of failing. Jorge would benefit reading about male sexuality and how he can develop his own sexual self (link resources)'
+          'Jorge’s ED is related to feelings of anxiety and the expectations his sets to perform sexually for his partner. Also, moving to a new country, and in an new relationship can introduce a number of challenges that can increase feelings of anxiety and the fear of failing. Jorge would benefit reading about male sexuality and how he can develop his own sexual self (see Resources section)'
       },
       {
         type: 'relational',
         text: 'Has a girlfriend of 6 months',
         intervention:
-          'Expectations from media, film, family, friends for men to always perform sexually, or to know how to “please” a partner. His girlfriend is understanding but not sure how to help him. Sensate focus exercises could help them both increase pleasure and reduce performance anxieties. Playing the game of “ten things I enjoy in sex” can be a fun way to open up converstaions about sex with a partner. The list can be regularly reviewed and items been added or removed, more like a sexual shopping list of what is on offer'
+          'Expectations from media, film, family, friends for men to always perform sexually, or to know how to “please” a partner. His girlfriend is understanding but not sure how to help him. Sensate focus exercises could help them both increase pleasure and reduce performance anxieties (see Resources section)'
       },
 
       {
         type: 'physical',
         text: 'Has no difficulties when he masturbates',
         intervention:
-          'Although when masturbating Jorge is able to stay in a physically pleasurable state of mind and body, when having sex with his partner his body goes into “fight or flight” mode, hutting down the pleasure centres in his brain that relate to maintain erection. Jorge would benefit to know more about his body’s responses to signals of stress and pleasure (appetitive and aversive states). Mindfulness could help him cultivate appetitive state. Kegel exercises could help Jorge become familiar with voluntary tensing and relaxing of the pelvic floor muscles that can hinder or facilitate erections'
+          'Although when masturbating Jorge is able to stay in a physically pleasurable state of mind and body, when having sex with his partner his body goes into “fight or flight” mode, shutting down the pleasure centres in his brain that relate to maintain erection. Jorge would benefit to know more about his body’s responses to signals of stress and pleasure (see Mechanism section). Mindfulness could help him cultivate appetitive state. Kegel exercises could help Jorge become familiar with voluntary tensing and relaxing of the pelvic floor muscles that can hinder or facilitate erections (see Resources)'
       }
     ]
   },
@@ -60,7 +60,7 @@ const SlidesData = [
     symptoms: [
       {
         type: 'relational',
-        text: 'His-year relationship to another man ended recently',
+        text: 'His 3-year relationship to another man ended recently',
         intervention:
           'The loss of a long-term relationship can bring feelings of loneliness, and affect our desire for sex, leading to ED. Experiences of bullying or rejection because of one’s sexuality by family, friends or other social groups can increase the fear of rejection when meeting new people. Martin could benefit from joining an LGBTQ social groups that foster belonging and a sense of community.'
       },
@@ -69,14 +69,14 @@ const SlidesData = [
         text:
           'Has used a mediction for ED but didn’t work. Takes anti-depressants',
         intervention:
-          'Anti-depressants can cause ED. Medications work only when a man is in an appetitive state of mind. Talking to his physician could help Martin make a change or adjustment to his anti-depressant medication. Reading about the way ED medications work with his body can help him use them effectively.'
+          'Anti-depressants can cause ED. Medications work only when a man is in an appetitive state of mind. Talking to his physician could help Martin make a change or adjustment to his anti-depressant medication. Reading about the way ED medications work with his body can help him use them effectively (see Resources)'
       },
 
       {
         type: 'psychological',
         text: 'Has casual sex but for the first time he can’t get an erection',
         intervention:
-          'The uncertainties of meeting casual partners can trigger personal insecurities and fears of being rejected than contribute to ED. Martin would benefit to know more about his body’s responses to signals of stress and pleasure (appetitive and aversive states). Mindfulness could help Martin cultivate emotional calmness particularly at times when he faces uncertainty'
+          'The uncertainties of meeting casual partners can trigger personal insecurities and fears of being rejected that contribute to ED. Martin would benefit to know more about his body’s responses to signals of stress and pleasure. Mindfulness could help Martin cultivate emotional calmness particularly at times when he faces uncertainty'
       }
     ]
   },
@@ -108,7 +108,7 @@ const SlidesData = [
   },
   {
     name: 'Mohamed',
-    about: '36 years old, 9 years married',
+    about: '36 years old',
     symptoms: [
       {
         type: 'relational',
@@ -121,7 +121,7 @@ const SlidesData = [
         text:
           'Although his wife learned how to manage pain, he now experiences ED',
         intervention:
-          'Previous experiences of pain by his partner during sex are likely to have triggered Mohamed’s aversive system (sympathetic), causing tension and physical discomfort in the thought of potentially harming/causing pain to his partner during sex. Mindfulness could help Mohamed to understand his fear response and learn how stay calm in his own body.'
+          'Previous experiences of pain by his partner during sex are likely to have triggered Mohamed’s aversive system (see Mechanism), causing tension and physical discomfort in the thought of potentially harming/causing pain to his partner during sex. Mindfulness could help Mohamed to understand his fear response and learn how stay calm in his own body.'
       },
       {
         type: 'psychological',
@@ -140,7 +140,7 @@ const SlidesData = [
         type: 'relational',
         text: 'Tends to avoid relationships',
         intervention:
-          'Ralph might need to discuss with a partner the possible impact of sexual abuse to their relationship in terms of feelings of fear about physical intimacy and relationships. Organisations such as relate.org.uk and tavistockrelationships.org provide support to couples that experience relational difficulties or challenges with intimacy.'
+          'Ralph might need to discuss with a partner the possible impact of sexual abuse in terms of feelings of fear about physical intimacy. Organisations such as relate.org.uk and tavistockrelationships.org provide support to couples that experience relational difficulties and challenges with intimacy.'
       },
       {
         type: 'physical',
